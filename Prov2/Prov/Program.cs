b46570aa68fc57f49b859e20c8cf5b68fddb397c
PhilipB20 <70744825+PhilipB20@users.noreply.github.com>
2021-02-18 15:07:13 +0100

    Add files via upload

---
+++ Prov2/Prov/Program.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System;
+
+namespace Prov
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Limpa;
+            ArrayList arlist1 = new ArrayList();
+            arlist1.Add("Mjölk");
+            arlist1.Add("Tårta");
+            arlist1.Add("Ost");
+            arlist1.Add("Skinka");
+            arlist1.Add("Limpa");
+
+            Random generator = new Random();
+            string Limpa2 = "";
+            Console.ReadLine();
+
+            if (Limpa2 != "Limpa")
+            {
+                Limpa = generator.Next(1 - 100);
+                Console.WriteLine("Limpa " + Limpa + " kg");
+            }
+
+            else
+            {
+                Console.WriteLine("Ogiltigt");
+            }
+
+
+
+
+
+
+        }
+    }
+}
